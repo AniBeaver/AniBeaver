0d31ef053380a225992f1a68cc36ae04b273d5bb
darius-it <179517889+darius-it@users.noreply.github.com>
2025-06-06 14:45:14 +0200

    chore: initial commit

---
+++ composeApp/src/commonMain/kotlin/org/anibeaver/anibeaver/Routes.kt
@@ -0,0 +1,8 @@
+package org.anibeaver.anibeaver
+
+enum class Screens(val title: String) {
+    Home(title = "AniBeaver"),
+    Anime(title = "Anime"),
+    Manga(title = "Manga"),
+    Settings(title = "Settings")
+}
---
+++ composeApp/src/commonMain/kotlin/org/anibeaver/anibeaver/ui/Home.kt
@@ -0,0 +1,44 @@
+package org.anibeaver.anibeaver.ui
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import org.anibeaver.anibeaver.Screens
+import org.anibeaver.anibeaver.ui.theme.Typography
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview
+fun HomeScreen(
+    navController: NavHostController = rememberNavController()
+) {
+    Column (modifier = Modifier.padding(32.dp)) {
+        Text("Welcome to AniBeaver", style = Typography.headlineLarge, modifier = Modifier.padding(bottom = 8.dp))
+
+        Button(onClick = {
+            navController.navigate(Screens.Anime.name)
+        }) {
+            Text("Go to Anime")
+        }
+
+        Button(onClick = {
+            navController.navigate(Screens.Manga.name)
+        }) {
+            Text("Go to Manga")
+        }
+
+        Button(onClick = {
+            navController.navigate(Screens.Settings.name)
+        }) {
+            Text("Go to Settings")
+        }
+    }
+
+
+}
