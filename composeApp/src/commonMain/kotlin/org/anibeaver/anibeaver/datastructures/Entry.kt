33062bc3c2e09f0119e24b62040cc80fd5c3356e
LS <lev.soltys@qaware.de>
2025-06-22 15:27:48 +0200

    feat: tag input 2

--- composeApp/src/commonMain/kotlin/org/anibeaver/anibeaver/datastructures/Entry.kt
+++ composeApp/src/commonMain/kotlin/org/anibeaver/anibeaver/datastructures/Entry.kt
@@ -1,16 +1,15 @@
 package org.anibeaver.anibeaver.datastructures
 
-//TODO: potentially tags to String[]
 class Entry internal constructor(
     val animeName: String,
     val releaseYear: String,
-    val studioName: String,
-    val genre: String,
+    val studioId: Int, // Tag id for studio
+    val genreIds: List<Int>, // Tag ids for genres
     val description: String,
     val rating: Float,
     val status: String,
     val releasingEvery: String,
-    val tags: String,
+    val tagIds: List<Int>, // Tag ids for custom tags
     internal val id: Int
 ) {
     fun getId(): Int = id
