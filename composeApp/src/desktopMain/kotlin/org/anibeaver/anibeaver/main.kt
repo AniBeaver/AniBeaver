a7cd359ab497735ccef5b2e0c1daea8e3f0bf5d4
Darius <179517889+darius-it@users.noreply.github.com>
2025-06-29 23:24:03 +0200

    refactor: remove unnecessary spaces and imports

--- composeApp/src/desktopMain/kotlin/org/anibeaver/anibeaver/main.kt
+++ composeApp/src/desktopMain/kotlin/org/anibeaver/anibeaver/main.kt
@@ -1,24 +1,22 @@
 package org.anibeaver.anibeaver
 
-import org.anibeaver.anibeaver.DataWrapper
-import org.anibeaver.anibeaver.api.ApiHandler
-import org.anibeaver.anibeaver.api.DesktopApiAuthorizationHandler
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
-
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import org.anibeaver.anibeaver.api.ApiHandler
+import org.anibeaver.anibeaver.api.DesktopApiAuthorizationHandler
 
 @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
 fun main() = application {
     val activityKiller: () -> Unit = {
         this.exitApplication()
     }
-    val dataWrapper : DataWrapper = DataWrapper(activityKiller, apiHandler = ApiHandler(DesktopApiAuthorizationHandler()))
+    val dataWrapper = DataWrapper(activityKiller, apiHandler = ApiHandler(DesktopApiAuthorizationHandler()))
     Window(
         onCloseRequest = ::exitApplication,
         title = "AniBeaver",
     ) {
-        App(dataWrapper = dataWrapper,windowSizeClass = calculateWindowSizeClass() )
+        App(dataWrapper = dataWrapper, windowSizeClass = calculateWindowSizeClass())
     }
 }
