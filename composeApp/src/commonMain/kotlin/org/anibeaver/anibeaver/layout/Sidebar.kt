06d0a5029d92d7a90516f85fbc322cedde215b05
darius-it <179517889+darius-it@users.noreply.github.com>
2025-06-30 16:56:48 +0200

    feat: extract sidebar nav items and wrap with tooltip

--- composeApp/src/commonMain/kotlin/org/anibeaver/anibeaver/layout/Sidebar.kt
+++ composeApp/src/commonMain/kotlin/org/anibeaver/anibeaver/layout/Sidebar.kt
@@ -83,7 +83,6 @@ fun Sidebar(
 }
 
 
-
 @Composable
 fun CenterSidebarEntries(
     navController: NavHostController = rememberNavController(),
@@ -91,19 +90,12 @@ fun CenterSidebarEntries(
     setDestination: (String) -> Unit
 ) {
     Screens.entries.filter { s -> s.position == NavItemPosition.Center }
-        .forEachIndexed { index, destination ->
-            NavigationRailItem(
-                selected = destination.name == selectedDestination,
-                onClick = {
-                    navController.navigate(route = destination.name)
-                    setDestination(destination.name)
-                },
-                icon = {
-                    Icon(
-                        destination.icon,
-                        contentDescription = destination.title,
-                    )
-                }
+        .forEach { destination ->
+            SidebarNavItem(
+                navController,
+                destination,
+                selectedDestination,
+                setDestination
             )
         }
 }
@@ -115,19 +107,45 @@ fun BottomSidebarEntries(
     setDestination: (String) -> Unit
 ) {
     Screens.entries.filter { s -> s.position == NavItemPosition.Bottom }
-        .forEachIndexed { index, destination ->
-            NavigationRailItem(
-                selected = destination.name == selectedDestination,
-                onClick = {
-                    navController.navigate(route = destination.name)
-                    setDestination(destination.name)
-                },
-                icon = {
-                    Icon(
-                        destination.icon,
-                        contentDescription = destination.title,
-                    )
-                }
+        .forEach { destination ->
+            SidebarNavItem(
+                navController,
+                destination,
+                selectedDestination,
+                setDestination
             )
         }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SidebarNavItem(
+    navController: NavHostController,
+    destination: Screens,
+    currentlySelectedDestination: String,
+    setDestination: (String) -> Unit
+) {
+    TooltipBox(
+        positionProvider = TooltipDefaults.rememberPlainTooltipPositionProvider(),
+        tooltip = {
+            PlainTooltip { Text(destination.name) }
+        },
+        state = rememberTooltipState()
+    ) {
+        NavigationRailItem(
+            selected = destination.name == currentlySelectedDestination,
+            onClick = {
+                if (destination.name != currentlySelectedDestination) {
+                    navController.navigate(route = destination.name)
+                    setDestination(destination.name)
+                }
+            },
+            icon = {
+                Icon(
+                    destination.icon,
+                    contentDescription = destination.title,
+                )
+            }
+        )
+    }
 }
