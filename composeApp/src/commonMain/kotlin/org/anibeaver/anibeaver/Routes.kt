67e5912e9bb3539746c3f89704645e2a7a91ed83
darius-it <179517889+darius-it@users.noreply.github.com>
2025-08-13 21:26:05 +0300

    chore: rename routes

--- composeApp/src/commonMain/kotlin/org/anibeaver/anibeaver/Routes.kt
+++ composeApp/src/commonMain/kotlin/org/anibeaver/anibeaver/Routes.kt
@@ -10,9 +10,9 @@ enum class NavItemPosition {
 }
 
 enum class Screens(val title: String, val icon: ImageVector, val position: NavItemPosition) {
-    Home(title = "AniBeaver", icon = Icons.Filled.Home, position = NavItemPosition.Center),
+    Home(title = "Home", icon = Icons.Filled.Home, position = NavItemPosition.Center),
     Anime(title = "Anime", icon = Icons.Filled.Videocam, position = NavItemPosition.Center),
     Manga(title = "Manga", icon = Icons.Filled.Book, position = NavItemPosition.Center),
-    Test(title = "Test", icon = Icons.Filled.AccountCircle, position = NavItemPosition.Bottom),
+    Account(title = "Account", icon = Icons.Filled.AccountCircle, position = NavItemPosition.Bottom),
     Settings(title = "Settings", icon = Icons.Filled.Settings, position = NavItemPosition.Bottom)
 }
